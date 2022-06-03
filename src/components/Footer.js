f8ee8c1cea8fe35e93b181cc8d77f5472bdb40e2
garydoesweb3 <garydoesweb3@gmail.com>
2022-06-03 07:50:53 -0500

    More customizing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <p><small>John Doe &copy; 2022</small></p>
+            <p><small>Gary Mills &copy; 2022</small></p>
         </footer>
     );
 }
