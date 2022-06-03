201775b5897cb1bbe1029071f891f51e21e724aa
garydoesweb3 <garydoesweb3@gmail.com>
2022-06-03 07:35:47 -0500

    Customizing portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import profile from '../assets/profile.png';
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Gary Mills" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>I'm Gary Mills</h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
